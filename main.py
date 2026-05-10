4bda5686a5d7207ff76d31a9e49bfaf1a0cfa9e6
JuanfLeon <jf.leon@uniandes.edu.co>
2026-05-10 16:56:55 -0500

    .

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def root():
 
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = db["proveedores"]
+    comentarios = db["proveedores"].find({id:bar_id})
     return comentarios
 
 @app.post('/bares/{bar_id}/comentarios')
