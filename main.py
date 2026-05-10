261f4f40d8613dc4d1a9f69d692c4b987669fdf8
JuanfLeon <jf.leon@uniandes.edu.co>
2026-05-10 16:23:57 -0500

    general

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ client = MongoClient(os.environ["MONGO_URI"])
 
 # TODO: conectarse a la base de datos Admonsis  
 # db = client["ISIS*******"]
-db = client["ISIS2304D27202610"]
+db = client["ISIS2304"]
 
 
 @app.get("/")
