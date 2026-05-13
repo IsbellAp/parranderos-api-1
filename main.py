2a1188cf9dd52507d1f0cf5320f4328ef010bd9b
JuanfLeon <jf.leon@uniandes.edu.co>
2026-05-13 06:50:35 -0500

    merge

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
 )
 
 #os.environ para despliegue. Descomente cuando ya probó todo local.
-client = MongoClient('MONGO_URI')
+client = MongoClient('mongodb://ISIS2304D27202610:PGfMXdt1XNyg@157.253.236.88:8087/')
 # TODO: conectarse al cluster Admonsis  
 
 
